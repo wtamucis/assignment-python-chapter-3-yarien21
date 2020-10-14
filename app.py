2d4c17db0ed46bd69b8fe54dacf34b3e5dfbf6f5
Sean Humpherys <shumpherys@wtamu.edu>
2020-10-14 13:29:40 -0500

    still at work

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
+# Yusmida Arien
+
 # Chapter 3.1 Introduction, no code
 # Chapter 3.2 Conditional Statements
+print("Chapter 3.2 Conditional Statements")
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+
+else:
+    print("It's cold")
+print("Done")
+print("*" * 30)
+
+# Chapter 3.3 Ternary Operator
+print("Chapter 3.3 Ternary Operator")
+age = 12
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
+
+cost = 300
+status = "Eligible" if cost > 100 else "cheap"
+print(status)
+print("*" * 30)
