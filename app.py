3b498ab90dea636c347decf3c24d7a247339dcaf
Sean Humpherys <shumpherys@wtamu.edu>
2020-10-19 16:54:49 -0500

    Ready for grading

--- app.py
+++ app.py
@@ -58,12 +58,12 @@ print("*" * 30)
 
 
 # Chapter 3.8 For Loops
-print("Chapter 3.8 For Loops")
+print("#Chapter 3.8 For Loops")
 for number in range(3):
-    print("Attempt", (number + 1), * ".")
+    print("Attempt", number + 1, (number + 1), * ".")
 
 for number in range(1, 4):
-    print("Attempt", (number + 1), * ".")
+    print("Attempt", number + 1, (number + 1), * ".")
 
 # How to count by twos
 for number in range(1, 10, 2):
@@ -100,10 +100,11 @@ print("*" * 30)
 
 
 # Chapter 3.10 Nested Loops
-print("Chapter 3.10 Nested Loops")
+print("#Chapter 3.10 Nested Loops")
 for x in range(5):
     for y in range(3):
         print(f"({x}, {y})")
+
 
 # Chapter 3.11 Iterables
 print("#Chapter 3.11 Iterables")
@@ -128,14 +129,15 @@ while True:
     print("ECHO", command)
     if command . lower() == "quit":
         break
-    print("*" * 30)
+print("*" * 30)
 
-    # Chapter 3.14 Exercise
-    print("#Chapter 3.14 Exercise")
-    counter = 0
-    for number in range(1, 10):
-        if number % 2 == 0:
-            print(number)
-            counter += 1
+
+# Chapter 3.14 Exercise
+print("#Chapter 3.14 Exercise")
+counter = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        print(number)
+        counter += 1
 print(f"we have {counter} even numbers")
 print("*" * 30)
